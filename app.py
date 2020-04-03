96ff4f65771ed44be4bb1afd39f8b78c95dd9b3a
idejie <i@idejie.com>
2020-04-04 00:58:32 +0800

    v0.2.1: 添加药品管理

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 
 def file_name(file_dir):
@@ -16,7 +16,9 @@ def file_name(file_dir):
     return L
 
 
-DIR_P = 'static/data'
+DIR_P = 'static/data/user'
+DIR_M = 'static/data/medicine'
+DIR_YF = 'static/data/recipe'
 
 
 def gen_p_id():
@@ -24,9 +26,156 @@ def gen_p_id():
     return max(L) + 1
 
 
+def gen_m_id():
+    L = file_name(DIR_M)
+    return max(L) + 1
+
+
+def gen_yf_id():
+    L = file_name(DIR_YF)
+    return max(L) + 1
+
+
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
+
+
+# 添加药品
+@app.route('/add/yp', methods=['GET', 'POST'])
+def add_yp():
+    if request.method == "GET":
+        return render_template('yaopin.html')
+    else:
+        name = request.form['name']
+        func = request.form['func']
+        desc = request.form['desc']
+        m_id = gen_m_id()
+        yp = {
+            'id': m_id,
+            'name': name,
+            'func': func,
+            'desc': desc
+        }
+        yp_f = os.path.join(DIR_M, str(m_id) + '.json')
+        with open(yp_f, 'w', encoding='utf8') as f:
+            json.dump(yp, f, ensure_ascii=False)
+        return render_template('yaopin.html', msg='药品【' + name + '】已添加。')
+
+
+# 查询药品
+@app.route('/query/yp', methods=['GET', 'POST'])
+def query_yp():
+    if request.method == "GET":
+        return render_template('query_yp.html')
+    else:
+        all_ = request.form['all']
+        name = request.form['name']
+        func = request.form['func']
+        desc = request.form['desc']
+
+        data = []
+        for root, dirs, files in os.walk(DIR_M):
+            for file in files:
+                file = os.path.join(DIR_M, file)
+                with open(file, 'r', encoding='utf8') as f:
+                    yp = json.load(f)
+                    if all_ == "true":
+                        data.append(yp)
+                    elif (name is not '' and name in yp.get('name', '')) \
+                            or (func is not '' and func in yp.get('func', '')) \
+                            or (desc is not '' and desc in yp.get('desc', '')):
+                        data.append(yp)
+            res = {'data': data}
+        return jsonify(res)
+
+
+# 删除药品
+@app.route('/del/yp', methods=['GET', 'POST'])
+def del_yp():
+    m_id = request.args.get('mid')
+    print(m_id)
+    yp_f = os.path.join(DIR_M, str(m_id) + '.json')
+    with open(yp_f, 'r', encoding='utf8') as f:
+        yp = json.load(f)
+    name = yp.get('name', '')
+    try:
+        os.remove(yp_f)
+        print(yp_f, "已删除")
+        return render_template('result.html', msg='药品【' + str(name) + '】,id【' + m_id + '】已删除。')
+    except OSError as e:
+        return render_template('result.html', msg=str(e))
+
+
+# 修改药品
+@app.route('/detail/yp', methods=['GET', 'POST'])
+def detail_yp():
+    if request.method == 'GET':
+        m_id = request.args.get('mid')
+        yp_f = os.path.join(DIR_M, str(m_id) + '.json')
+        with open(yp_f, 'r', encoding='utf8') as f:
+            yp = json.load(f)
+        return render_template('detail_yp.html', yp=yp)
+    else:
+        m_id = request.form['mid']
+        print(m_id)
+        name = request.form['name']
+        func = request.form['func']
+        desc = request.form['desc']
+        yp = {
+            'id': m_id,
+            'name': name,
+            'func': func,
+            'desc': desc
+        }
+        yp_f = os.path.join(DIR_M, str(m_id) + '.json')
+        with open(yp_f, 'w', encoding='utf8') as f:
+            json.dump(yp, f, ensure_ascii=False)
+        return render_template('result.html', msg='药品【' + name + '】,id【' + str(m_id) + '】修改。')
+
+
+# 增加药方
+@app.route('/add/yf', methods=['GET', 'POST'])
+def add_recipe():
+    if request.method == 'GET':
+        return render_template('yaofang_add.html', msg='')
+    else:
+        print(request.form)
+        name = request.form['name']
+        yf = request.form['yf']
+        yf_id = gen_yf_id()
+        yaofang = {
+            "id": yf_id,
+            "name": name,
+            "zz": "",
+            "cases": [
+                {'zz': "", 'yf': yf}
+            ]
+        }
+        yf_f = os.path.join(DIR_YF, str(yf_id) + '.json')
+        with open(yf_f, 'w', encoding='utf8') as f:
+            json.dump(yaofang, f, ensure_ascii=False)
+        return render_template('yaofang_add.html', msg='ss')
+
+
+# 查询药方
+@app.route('/query/yf', methods=['GET'])
+def query_recipe():
+    zz = request.args.get('zz')
+    if not zz:
+        return jsonify({'data': []})
+    data = []
+    for root, dirs, files in os.walk(DIR_YF):
+        for file in files:
+            file = os.path.join(DIR_YF, file)
+            print(file)
+            with open(file, 'r', encoding='utf8') as f:
+                yf = json.load(f)
+                if zz in yf.get('zz'):
+                    data.append(yf)
+        res = {'data': data}
+        print(res)
+    return jsonify(res)
 
 
 @app.route('/detail', methods=['GET'])
@@ -36,11 +185,11 @@ def detail():
     with open(p_f, 'r', encoding='utf8') as f:
         data = json.load(f)
 
-    return render_template('detail.html', data=data, p_id=p_id)
+    return render_template('detail_br.html', data=data, p_id=p_id)
 
 
 # 增
-@app.route('/add/person', methods=['GET', 'POST'])
+@app.route('/add/br', methods=['GET', 'POST'])
 def add_person():
     if request.method == 'POST':
         name = request.form['name']
@@ -57,32 +206,10 @@ def add_person():
         }
         p_f = os.path.join(DIR_P, str(p_id) + '.json')
         with open(p_f, 'w', encoding='utf8') as f:
-            json.dump(person, f)
+            json.dump(person, f, ensure_ascii=False)
         return redirect(url_for('detail', p_id=p_id))
     else:
         return render_template('person.html')
-
-
-@app.route('/add/recipe', methods=['GET', 'POST'])
-def add_recipe():
-    if request.method == 'GET':
-        return render_template('recipe.html', msg='')
-    else:
-        zz = request.form['zz']
-        yf = request.form['yf']
-        p_id = gen_p_id()
-        person = {
-            "name": '',
-            "phone": '',
-            "cases": [
-                {'zz': zz, 'yf': yf}
-            ]
-        }
-        p_f = os.path.join(DIR_P, str(p_id) + '.json')
-        with open(p_f, 'w', encoding='utf8') as f:
-            json.dump(person, f)
-        return render_template('recipe.html',
-                               msg='成功新增一药方：症状为【' + person['cases'][0]['zz'] + '】,药方为【' + person['cases'][0]['yf'] + '】')
 
 
 @app.route('/add/case', methods=['GET', 'POST'])
@@ -95,40 +222,8 @@ def add_case():
         data = json.load(f)
     data['cases'].append({'zz': zz, 'yf': yf})
     with open(p_f, 'w', encoding='utf8') as f:
-        json.dump(data, f)
+        json.dump(data, f, ensure_ascii=False)
     return redirect(url_for('detail', p_id=p_id))
-
-
-# 删
-@app.route('/del/person', methods=['POST'])
-def del_person():
-    return 'hello'
-
-
-@app.route('/del/recipe', methods=['POST'])
-def del_recipe():
-    return 'hello'
-
-
-@app.route('/del/case', methods=['POST'])
-def del_case():
-    return 'hello'
-
-
-# 改
-@app.route('/update/person', methods=['POST'])
-def update_person():
-    return 'hello'
-
-
-@app.route('/update/recipe', methods=['POST'])
-def update_recipe():
-    return 'hello'
-
-
-@app.route('/update/case', methods=['POST'])
-def update_case():
-    return 'hello'
 
 
 # 查
@@ -149,24 +244,6 @@ def query_person():
                         data.append({'name': name_t, "p_id": p_id})
         res = {'data': data}
         return jsonify(res)
-
-
-@app.route('/query/yf', methods=['GET'])
-def query_recipe():
-    zz = request.args.get('zz')
-    if not zz:
-        return jsonify({'data': []})
-    data = []
-    for root, dirs, files in os.walk(DIR_P):
-        for file in files:
-            file = os.path.join(DIR_P, file)
-            with open(file, 'r', encoding='utf8') as f:
-                cases = json.load(f).get('cases')
-                for case in cases:
-                    if zz in case['zz']:
-                        data.append(case)
-        res = {'data': data}
-    return jsonify(res)
 
 
 @app.route('/query/zz', methods=['GET'])
